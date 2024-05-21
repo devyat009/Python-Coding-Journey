3fb91db6a5bbb5e7f2eebd3a79cc72751f279772
Higor S <91706455+devyat009@users.noreply.github.com>
2024-05-21 15:55:16 -0300

    New question added along def file

---
+++ 20-05-24/def_criadas.py
@@ -0,0 +1,73 @@
+# Função para checar se o input do usuário é número:
+def e_numero(string):
+    try:
+        if string.isnumeric():
+            string = int(string)
+            return True   
+        else:
+            string = float(string)
+            return True # Colocamos False para termos apenas valores inteiros
+    except ValueError:
+        return False
+
+# Função para checar se o input do usuário é uma string:
+def e_letra(string):
+    try:
+        if string.isalpha():
+            string = str
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
+# Função para dizer qual é o tipo de terreno:
+def tipo(string):
+    try:
+        if string <= 100:
+            return "TERRENO POPULAR"
+        elif 100 > string < 500:
+            return "TERRENO MASTER"
+        elif string >= 500:
+            return "TERRENO VIP"
+    except ValueError:
+        False
+
+# Função para checar se o ano inserido é bissexto:
+def bissexto(string):
+    try: 
+        # Caso termine em '0' retorna verdadeiro:
+        if str(string).endswith("0"):
+            return True
+        # Caso seja divisível por '4' retorna verdadeiro:
+        elif int(string) % 4 == 0:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
+# Como sao chamadas
+
+# e numero
+if e_numero(string):
+    print("e uma letra")
+else:
+    print("nao e uma letra")
+
+# e letra
+if e_letra(string):
+    print("e uma letra")
+else:
+    print("nao e uma letra")
+
+# tipo
+m2 = 150
+tipa = tipo(m2)
+print(tipa)
+
+# bisseexto
+if bissexto(ano):
+    print("O ano é bissexto.")
+else:
+    print("Não é um ano bissexto.")
---
+++ 20-05-24/questao_14.py
@@ -0,0 +1,8 @@
+# Atividade: [DESAFIO] Refaça o algoritmo 25, acrescentando o recurso de mostrar que tipo
+# de triângulo será formado:
+# - EQUILÁTERO: todos os lados iguais
+# - ISÓSCELES: dois lados iguais
+# - ESCALENO: todos os lados diferentes
+# Data: 20/05/2024
+# Instutor: Bruno
+# Autor: Higor Stanley
