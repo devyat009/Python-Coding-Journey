3eef5ff491a414dc04d0863c1281c53c2128a216
Higor S <91706455+devyat009@users.noreply.github.com>
2024-05-22 15:15:00 -0300

    Question 14 and 15

--- 20-05-24/questao_14.py
+++ 20-05-24/questao_14.py
@@ -3,6 +3,19 @@
 # - EQUILÁTERO: todos os lados iguais
 # - ISÓSCELES: dois lados iguais
 # - ESCALENO: todos os lados diferentes
-# Data: 20/05/2024
+# Data: 22/05/2024
 # Instutor: Bruno
 # Autor: Higor Stanley
+
+# Função para checar se o input do usuário é número:
+def e_numero(string):
+    try:
+        if string.isnumeric():
+            string = int(string)
+            return True   
+        else:
+            string = float(string)
+            return True # Colocamos False para termos apenas valores inteiros
+    except ValueError:
+        return False
+
