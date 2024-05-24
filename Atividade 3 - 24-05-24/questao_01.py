3cecb3a8b6eb7249a4a6882bf51bfe85e8e7dd69
Higor S. <91706455+devyat009@users.noreply.github.com>
2024-05-24 20:17:56 +0000

    New questions added

---
+++ Atividade 3 - 24-05-24/questao_01.py
@@ -0,0 +1,15 @@
+# Atividade: Escreva um programa que mostre na tela as seguintes contagens:  
+# 0 
+# 1 
+# 2 
+# 3 
+# 4 
+# 5 
+# Data: 24/05/2024
+# Instutor: Bruno
+# Autor: Higor Stanley
+
+
+# Print da i
+for i in range(0, 6):
+    print(i)
