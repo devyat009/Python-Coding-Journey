9400b3a8f7296521383f5e2355ae405306a9ea56
Higor S. <91706455+devyat009@users.noreply.github.com>
2024-05-27 17:44:29 +0000

    Sync

---
+++ Atividade 3 - 24-05-24/questao_08.py
@@ -0,0 +1,7 @@
+# Atividade: Faça um aplicativo que leia o preço de 8 produtos. No final, mostre na tela  
+# qual foi o maior e qual foi o menor preço digitado. 
+# Data: 27/05/2024
+# Instrutor: Brunno
+# Autor: Higor Stanley
+
+quantidade = float(input('Insira a quantidade de produtos que dejesa inserir o preço: '))
