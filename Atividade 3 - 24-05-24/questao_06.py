7beaafe2e34caa92d3bd3075b4acf5b15add1c58
Higor S. <91706455+devyat009@users.noreply.github.com>
2024-05-29 19:47:08 +0000

    Input validation fix in some files

--- Atividade 3 - 24-05-24/questao_06.py
+++ Atividade 3 - 24-05-24/questao_06.py
@@ -4,19 +4,49 @@
 # Instutor: Bruno
 # Autor: Higor Stanley
 
-# Input do usuário:
-numero = int(input('Insira a quantidade de números que deseja escrever: '))
+# Função para checar se o input do usuário é número:
+def e_numero(string):
+    try:
+        if string.isnumeric():
+            string = int(string)
+            return True   
+        else:
+            string = float(string)
+            return True # Colocamos False para termos apenas valores inteiros
+    except ValueError:
+        return False
+
+# Listas:
 lista_par = []
 lista_impar = []
 
-# Repetição para inserir os números:
-for i in range(0, numero):
-    insira = int(input(f'Insira o número {numero+1}:'))
-    if insira % 2 == 0:
-        lista_par.append(insira)
+# Loop do programa para validações:
+while True:
+    # Input do usuário:
+    numero = input('Insira a quantidade de números que deseja escrever: ')
+    if e_numero(numero):
+        numero = int(numero)
+        # Repetição para inserir os números:
+        for i in range(0, numero):
+            while True:
+                insira = input(f'Insira o número {i+1}:')
+                if e_numero(insira):
+                    insira = int(insira)
+                    if insira % 2 == 0:
+                        lista_par.append(insira)
+                        break
+                    else:
+                        lista_impar.append(insira)
+                        break
+                # Caso o input não seja numérico:
+                else:
+                    print("\nALERTA! Insira um valor com números. Tente novamente!")
+            
+        # Resultado do programa:
+        print(f'Este são os números pares:{lista_par}')
+        print(f'Estes são os números ímpares:{lista_impar}')
+        #Sair do loop   
+        break
+    # Caso o input não seja numérico:
     else:
-        lista_impar.append(insira)
-    
-# Resultado do programa:
-print(f'Este são os números pares:{lista_par}')
-print(f'Estes são os números ímpares:{lista_impar}')
+        print("\nALERTA! Insira um valor com números. Tente novamente!")
--- Atividade 3 - 24-05-24/questao_09.py
+++ Atividade 3 - 24-05-24/questao_09.py
@@ -7,22 +7,54 @@
 # Instrutor: Brunno
 # Autor: Higor Stanley
 
-# Quantidade de pessoas em que o usuário ira escolher:
-quantidade = int(input('Insira a quantidade de pessoas: '))
+# Função para checar se o input do usuário é número:
+def e_numero(string):
+    try:
+        if string.isnumeric():
+            string = int(string)
+            return True   
+        else:
+            string = float(string)
+            return True # Colocamos False para termos apenas valores inteiros
+    except ValueError:
+        return False
+# Lista
 pessoas = []
 
-for i in range(0,quantidade):
-    idade = int(input(f'Insira a idade da pessoa {i+1}: '))
-    pessoas.append(idade)
+# Loop do programa para validações:
+while True:
+    # Quantidade de pessoas em que o usuário ira escolher:
+    quantidade = input('Insira a quantidade de pessoas: ')
+    if e_numero(quantidade):
+        quantidade = int(quantidade)
+        for i in range(0,quantidade):
+            while True:
+                idade = input(f'Insira a idade da pessoa {i+1}: ')
+                if e_numero(idade):
+                    idade = int(idade)
+                    pessoas.append(idade)
+                    break
+                # Caso o input não seja numérico:
+                else:
+                    print("\nALERTA! Insira um valor com números. Tente novamente!")
+        # Calculamos a média:
+        media = sum(pessoas) / quantidade
+        print(f'A média: {media:.2f}')\
 
-# Calculamos a média:
-media = sum(pessoas) / quantidade
-print(f'A média: {media:.2f}')\
+        # Descobrimos quantos são maiores de idade:
+        maior_de_dezoito = sum(1 for idade in pessoas if idade >= 18)
+        print(f'Tem {maior_de_dezoito} maiores de 18 anos de idade.')
 
-# Descobrimos quantos são maiores de idade:
-maior_de_dezoito = sum(1 for idade in pessoas if idade >= 18)
-print(f'Tem {maior_de_dezoito} maiores de 18 anos de idade.')
+        # Descobrimos quantos são tem menos de 5 anos
+        menor_de_cinco = sum(1 for idade in pessoas if idade < 5)
+        print(f'Tem {menor_de_cinco} com menos de 5 anos de idade.')
+        
+        #Sair do loop
+        break
+    # Caso o input não seja numérico:
+    else:
+        print("\nALERTA! Insira um valor com números. Tente novamente!")
 
-# Descobrimos quantos são tem menos de 5 anos
-menor_de_cinco = sum(1 for idade in pessoas if idade >= 18)
-print(f'Tem {menor_de_cinco} com menos de 5 anos de idade.')
+    
+
+    
