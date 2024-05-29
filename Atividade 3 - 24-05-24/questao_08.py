9b23c82fa9cff9f5653f03c6462e8cfcc5d6cf9b
Higor S. <91706455+devyat009@users.noreply.github.com>
2024-05-29 20:03:51 +0000

    Revert "Input validation"
    This reverts commit 7beaafe2e34caa92d3bd3075b4acf5b15add1c58.

--- Atividade 3 - 24-05-24/questao_08.py
+++ Atividade 3 - 24-05-24/questao_08.py
@@ -4,44 +4,15 @@
 # Instrutor: Brunno
 # Autor: Higor Stanley
 
-# Função para checar se o input do usuário é número:
-def e_numero(string):
-    try:
-        if string.isnumeric():
-            string = int(string)
-            return True   
-        else:
-            string = float(string)
-            return True # Colocamos False para termos apenas valores inteiros
-    except ValueError:
-        return False
-
-# Lista:
+# Pede as informações para o usuário:
+quantidade = int(input('Insira a quantidade de produtos que dejesa inserir o preço: '))
 produtos = []
 
-# Loop do programa para validações:
-while True:
-    # Loop do programa para validações:
-    # Pede as informações para o usuário:
-    quantidade = input('Insira a quantidade de produtos que dejesa inserir o preço: ')
-    if e_numero(quantidade):
-        quantidade = int(quantidade)
-        for i in range(0, quantidade):
-            while True:
-                produ = input(f'Insira o preço do produto {i+1}: R$ ')
-                if e_numero(produ):
-                    produ = float(produ)
-                    produtos.append(produ)
-                    produtos.sort()
-                    break
-                # Caso o input não seja numérico:
-                else:
-                    print("\nALERTA! Insira um valor com números. Tente novamente!")
-        # A respota do programa:
-        print(f'O menor preço foi de: R${produtos[0]}')
-        print(f'O maior preço foi de: R${produtos[-1]}')
-        #Sair do loop
-        break
-    # Caso o input não seja numérico:
-    else:
-        print("\nALERTA! Insira um valor com números. Tente novamente!")
+for i in range(0, quantidade):
+    produ = float(input(f'Insira o preço do produto {i+1}: R$ '))
+    produtos.append(produ)
+    produtos.sort()
+
+# A respota do programa:
+print(f'O menor preço foi de: R${produtos[0]}')
+print(f'O maior preço foi de: R${produtos[-1]}')
--- Atividade 3 - 24-05-24/questao_09.py
+++ Atividade 3 - 24-05-24/questao_09.py
@@ -7,54 +7,22 @@
 # Instrutor: Brunno
 # Autor: Higor Stanley
 
-# Função para checar se o input do usuário é número:
-def e_numero(string):
-    try:
-        if string.isnumeric():
-            string = int(string)
-            return True   
-        else:
-            string = float(string)
-            return True # Colocamos False para termos apenas valores inteiros
-    except ValueError:
-        return False
-# Lista
+# Quantidade de pessoas em que o usuário ira escolher:
+quantidade = int(input('Insira a quantidade de pessoas: '))
 pessoas = []
 
-# Loop do programa para validações:
-while True:
-    # Quantidade de pessoas em que o usuário ira escolher:
-    quantidade = input('Insira a quantidade de pessoas: ')
-    if e_numero(quantidade):
-        quantidade = int(quantidade)
-        for i in range(0,quantidade):
-            while True:
-                idade = input(f'Insira a idade da pessoa {i+1}: ')
-                if e_numero(idade):
-                    idade = int(idade)
-                    pessoas.append(idade)
-                    break
-                # Caso o input não seja numérico:
-                else:
-                    print("\nALERTA! Insira um valor com números. Tente novamente!")
-        # Calculamos a média:
-        media = sum(pessoas) / quantidade
-        print(f'A média: {media:.2f}')\
+for i in range(0,quantidade):
+    idade = int(input(f'Insira a idade da pessoa {i+1}: '))
+    pessoas.append(idade)
 
-        # Descobrimos quantos são maiores de idade:
-        maior_de_dezoito = sum(1 for idade in pessoas if idade >= 18)
-        print(f'Tem {maior_de_dezoito} maiores de 18 anos de idade.')
+# Calculamos a média:
+media = sum(pessoas) / quantidade
+print(f'A média: {media:.2f}')\
 
-        # Descobrimos quantos são tem menos de 5 anos
-        menor_de_cinco = sum(1 for idade in pessoas if idade < 5)
-        print(f'Tem {menor_de_cinco} com menos de 5 anos de idade.')
-        
-        #Sair do loop
-        break
-    # Caso o input não seja numérico:
-    else:
-        print("\nALERTA! Insira um valor com números. Tente novamente!")
+# Descobrimos quantos são maiores de idade:
+maior_de_dezoito = sum(1 for idade in pessoas if idade >= 18)
+print(f'Tem {maior_de_dezoito} maiores de 18 anos de idade.')
 
-    
-
-    
+# Descobrimos quantos são tem menos de 5 anos
+menor_de_cinco = sum(1 for idade in pessoas if idade >= 18)
+print(f'Tem {menor_de_cinco} com menos de 5 anos de idade.')
