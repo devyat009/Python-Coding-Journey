acce89174b6821580986d9fb69a5a4c339063180
Higor S. <91706455+devyat009@users.noreply.github.com>
2024-06-06 19:32:02 +0000

    New File

---
+++ Atividade 4 - 04-06-24/Dicionarios.py
@@ -0,0 +1,338 @@
+# Atividade:
+# 1 - Crie um dicionário representando informações sobre uma pessoa, como nome, idade e cidade. 
+# 2 - Utilizando o dicionário criado no item 1:  
+# Modifique o valor de um dos itens no dicionário (por exemplo, atualize a idade da pessoa); 
+# Adicione um campo de profissão para essa pessoa; 
+# Remova um item do dicionário. 
+# 3 - Crie um dicionário para representar números e seus quadrados de 1 a 5. 
+# 4 - Crie um dicionário que verifique se uma chave específica existe dentro desse dicionário. 
+# 5 - Escreva um código que conte a frequência de cada palavra em uma frase utilizando um dicionário.
+#
+# Instrutor: Brunno
+# Data: 04/06/2024
+# Autor: Higor Stanley aka Devyat009
+# Programa: Dicionario
+
+# def adicionar_pessoas(pessoas):
+    # nome = input('▶ Digite o nome: ')
+    # idade = input('▶ Digite qual é a idade da pessoa: ')
+    # profissao = str(input('▶ Qual é a profissão dessa pessoa: '))
+    # print(f'Pessoa 1: Nome - {nome[0]} Idade - {idade} Profissão - {profissao}')
+
+# ⫷⫷⫷⫷⫷⫷⫷⫷ FUNÇÕES ⫸⫸⫸⫸⫸⫸⫸⫸⫸
+# Função para checar se o input do usuário é número:
+def e_numero(string):
+    try:
+        if string.isnumeric():
+            string = int(string)
+            return True   
+        else:
+            string = float(string)
+            return False # Colocamos False para termos apenas valores inteiros
+    except ValueError:
+        return False
+    
+# Função para checar se o input do usuário é uma string:
+def e_letra(string):
+    try:
+        if string.isalpha():
+            string = str
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
+
+# QUESTAO 2
+dados_pessoais = [
+        {'nome': 'Higor', 'idade': '22', 'profissao': ' ', 'cidade': 'sobradisley'},
+        {'nome': 'Luiz', 'idade': '21', 'profissao': ' ', 'cidade': 'sobradisley'}
+    ]
+
+def pessoas():
+    # Verifica se existem pessoas na lista:
+    if not dados_pessoais:
+        print('◇ A lista está vazia!')
+        return
+    # Caso contenha, imprime o seguinte:
+    
+    print('◇ A lista contem as seguintes pessoas: ')
+    for i, pessoa in enumerate(dados_pessoais, start=1):
+        print(f'◇ Pessoa {i}: Nome: {pessoa["nome"]}, Idade: {pessoa["idade"]}, Profissão: {pessoa["profissao"]}')
+    
+def alterar_idade_pessoas(dados_pessoais):
+    print('◇ Escolha a pessoa que deseja alterar: ')
+    pessoas()
+
+    alvo = int(input('▶ Escolha o numero da pessoa que deseja alterar: '))
+    nova_idade = int(input(f'▶ Insira a nova idade de {dados_pessoais[alvo - 1]["nome"]}: '))
+
+    dados_pessoais[alvo -1]['idade'] = nova_idade
+    print(f'◇ A nova idade de {dados_pessoais[alvo - 1]["nome"]} é {dados_pessoais[alvo -1]["idade"]}') 
+    
+def adicionar_profissao_pessoas(dados_pessoais):
+    print('◇ Escolha a pessoa que deseja alterar: ')
+    pessoas()
+
+    alvo = int(input('▶ Escolha o numero da pessoa que deseja alterar: '))
+    nova_profissao = input(f'▶ Insira a nova profissão de {dados_pessoais[alvo - 1]["nome"]}: ')
+    dados_pessoais[alvo -1]['profissao'] = nova_profissao
+    print(f'◇ A nova profissão de {dados_pessoais[alvo - 1]["nome"]} é {dados_pessoais[alvo -1]["profissao"]}') 
+
+def deletar_pessoas():
+    print('◇ Escolha a pessoa que deseja alterar: ')
+    pessoas()
+    # Caso tenha, tentar:
+    try:
+        # Usuario escolhe o estabelecimento alvo da lista:
+        alvo = int(input('▶ Digite o número da pessoa em que deseja remover: '))
+        # Se numero alvo do estabelecimento for menor que 0
+        if 0 < alvo <= len(dados_pessoais):
+            print('◇ Você confirma sua escolha? LEMBRE-SE NÃO É POSSÍVEL REVERTER (s/n)')
+            escolha = input('▶ Escolha: ').lower()
+            if escolha == 's':
+                # Usamos rest_alvo - 1 pois o for começa do 0
+                pessoa_removida = dados_pessoais.pop(alvo - 1)
+                print(f'◇ A pessoa {pessoa_removida["nome"]} foi removido com SUCESSO! ✓')
+            elif escolha == 'n':
+                print('◇ Cancelando...')
+            else:
+                print('⚠︎ Insira um valor valido!')
+    except ValueError:
+        print('⚠︎ Insira um valor valido!')
+
+def sub_menu_pessoas():
+    while True:
+        print('┏━╾──────────────────────────────────╼━┓')
+        print('┃〔 1  ☰  Listar pessoas           〕  ┃')
+        print('┃〔 2  ✎  Alterar a idade          〕  ┃')
+        print('┃〔 3  ✎  Adiconar a profissão     〕  ┃')
+        print('┃〔 4  ✗  Deletar uma pessoa       〕  ┃')
+        print('┃〔 5  ⇦  Voltar ao menu principal 〕  ┃')
+        print('┗━╾──────────────────────────────────╼━┛')
+        
+        escolha = input('▶ Sua escolha é: ')
+        if e_numero(escolha):
+            escolha = int(escolha)
+            match escolha:
+                case 1:
+                    pessoas()
+                case 2:
+                    alterar_idade_pessoas(dados_pessoais)
+                case 3:
+                    adicionar_profissao_pessoas(dados_pessoais)
+                case 4:
+                    deletar_pessoas()
+                case 5:
+                    # Sair do loop e voltar ao menu principal
+                    break
+                case _:
+                    print('⚠︎ Opção invalida, tente novamente!')
+        else:
+            print('⚠︎ Insira um número correspondente a uma opção válida!')
+
+# QUESTAO 3
+
+# Escolher número:
+def alterar_numero():
+        x = input('▶ Escolha um número para saber o quadrado: ')
+        while True:
+            if e_numero(x): 
+                x = int(x)
+                for i in range(1, x+1):
+                    print(f'◇ O quadrado de {i} é: {i**2}')
+                numeros = {x:x**2 for x in range(1,x+1)}
+                print(f'◇ Resultando um dicionario: {numeros}')
+                return numeros
+                #break
+            else:
+                print('⚠︎ Insira um número correspondente a uma opção válida!')
+
+# Menu dos Números:
+def numeros_quadrado():
+    
+    numeros_ao_quadrado = []
+    numeros_ao_quadrado.append(alterar_numero())
+    while True:
+        print('┏━╾──────────────────────────────────╼━┓')
+        print('┃〔 1  ☰  Listar dicíonario          〕┃')
+        print('┃〔 2  ✎  Alterar o número           〕┃')
+        print('┃〔 3  ⇦  Voltar ao menu principal   〕┃')
+        print('┗━╾──────────────────────────────────╼━┛')
+        
+        escolha = input('▶ Sua escolha é: ')
+        if e_numero(escolha):
+            escolha = int(escolha)
+            match escolha:
+                case 1:
+                    print(numeros_ao_quadrado)
+                case 2:
+                    alterar_numero()
+                case 3:
+                    # Sair do loop e voltar ao menu principal
+                    break
+                case _:
+                    print('⚠︎ Opção invalida, tente novamente!')
+        else:
+            print('⚠︎ Insira um número correspondente a uma opção válida!')
+
+# QUESTÃO 4
+# Adicona um dicionario  a partir de uma frase sem regras para a questão 4 e 5:
+lista_dicionario = []
+
+# Adiconar varias frases para o dicionario:
+def adicionar_dicionario():
+    dicionario = {}
+    while True:
+        frase_customizada = input('▶ Escreva a frase para adicionar ao dicionario: ').lower()
+        dicionario[frase_customizada] = dicionario.get(frase_customizada, 0) + 1
+        lista_dicionario.append(dicionario)  # Adiciona o novo dicionário à lista
+        continuar = input("◇ Deseja adicionar mais frases? (s/n): ").lower()
+        if continuar != 's':
+            return dicionario
+
+# Verifica se existe uma palavra no dicionario adicionado:
+# def verificar_palavra(dicionario):
+#     # Identifica se o dicionário está vazio:
+#     if not dicionario:
+#         print('⚠︎ O dicionário está vazio.')
+#         return
+#     # Identifica o input do usuário:
+#     while True:
+#         chave = input('▶ Escolha um nome para a palavra chave: ').strip().lower() # Remove o espacos em branco e deixa em minusculo
+#         if e_letra(chave):
+#             chave = str(chave)
+#             # Verifica quantas vezes foi encotrado no dicionário:
+#             if chave in dicionario:
+#                 print(f'⋄ A palavra chave "{chave}" foi encontrada {dicionario[chave]} vezes.')
+#                 break
+#             else:
+#                 print('⚠︎ Palavra chave não encontrada!')
+#                 break
+#         else:
+#             print('⚠︎ Insira uma letra ou uma palavra!')
+            
+            
+def verificar_palavra(dicionario):
+    # Identifica se o dicionário está vazio:
+    if not dicionario:
+        print('⚠︎ O dicionário está vazio.')
+        return
+    
+    # Identifica o input do usuário:
+    while True:
+        chave = input('▶ Escolha uma palavra chave: ').strip().lower()  # Remove espaços em branco e converte para minúsculas
+        if e_letra(chave):
+            #chave = str(chave) # LINHA 
+            # Verifica quantas vezes foi encontrada no dicionário:
+            if chave in dicionario:
+                print(f'◇ A palavra chave "{chave}" foi encontrada {dicionario[chave]} vezes.')
+                break
+            else:
+                print('⚠︎ Palavra chave não encontrada!')
+                break
+        else:
+            print('⚠︎ Insira uma palavra válida!')
+
+
+
+
+# listar o dicionario:          
+def listar_dicionario(dicionario):
+    print('◇ A lista do dicionario contem: ')
+    for i, (frase, frequencia) in enumerate(dicionario.items(), start=1):
+        print(f'◇ Frase {i}: {frase} - Frequência da frase: {frequencia}')
+# QUESTÃO 5
+def contar_frequencia(dicionario):
+    frequencia_palavras = {}
+
+    # Iterar sobre cada frase no dicionário
+    for frase, freq in dicionario.items():
+        # Dividir a frase em palavras
+        palavras = frase.split()
+        
+        # Iterar sobre cada palavra na frase
+        for palavra in palavras:
+            palavra = palavra.lower()  # Convertendo a palavra para minúsculas
+            if palavra in frequencia_palavras:
+                # Se a palavra já estiver no dicionário de frequência, incrementar sua contagem
+                frequencia_palavras[palavra] += freq
+            else:
+                # Se a palavra não estiver no dicionário de frequência, adicioná-la com a frequência correspondente
+                frequencia_palavras[palavra] = freq
+    
+    return frequencia_palavras
+    
+    # for palavra, freq in dicionario.items():
+    #     print(f"{palavra}: {freq}")
+        
+    
+# Menu Questão 4 e 5 - Dicionário:
+def menu_dicionario():
+    while True:
+        print('┏━╾──────────────────────────────────╼━┓')
+        print('┃〔 1  ＋Dicionário                 〕 ┃')
+        print('┃〔 2  ⌨ Verificar palavra          〕 ┃')
+        print('┃〔 3  ☲ Contar frequência          〕 ┃')
+        print('┃〔 4  ☲ Listar dicionário          〕 ┃')
+        print('┃〔 5  ⇦  Voltar ao menu principal  〕 ┃')
+        print('┗━╾──────────────────────────────────╼━┛')
+
+        # Dicionario vazio
+        
+        escolha = input('▶ Sua escolha é: ')
+        if e_numero(escolha):
+            escolha = int(escolha)
+            match escolha:
+                case 1:
+                    dicionario = adicionar_dicionario()
+                case 2:
+                    verificar_palavra(dicionario)
+                case 3:
+                    frequencia = contar_frequencia(dicionario)
+                    print(frequencia)
+                case 4:
+                    listar_dicionario(dicionario)
+                case 5:
+                    # Sair do loop e voltar ao menu principal
+                    break
+                case _:
+                    print('⚠︎ Opção inválida, tente novamente!')
+        else:
+            print('⚠︎ Insira um número correspondente a uma opção válida!')
+
+# Menu Principal
+def menu():
+    print('┏━╾──────────────────────────────────╼━┓')
+    print('┃〔 1  ＋Pessoas                 〕    ┃')
+    print('┃〔 2  ☰ Números                 〕    ┃')
+    print('┃〔 3  ☲ Dicionário              〕    ┃')
+    print('┃〔 4  ⤾ Sair do aplicativo      〕    ┃')
+    print('┗━╾──────────────────────────────────╼━┛')
+
+# Launcher do Programa
+def main():
+    while True:
+        #nome_programa()
+        menu()
+        escolha_opcao = input('▶ Sua escolha é: ')
+        if e_numero(escolha_opcao):
+            escolha_opcao = int(escolha_opcao)
+            match escolha_opcao:
+                case 1:
+                    sub_menu_pessoas()
+                case 2:
+                    numeros_quadrado()
+                case 3:
+                    menu_dicionario()
+                case 4:
+                    print('◇ Saindo.....')
+                    return  # Sair da função main e terminar o programa:
+                case _:
+                    print('⚠︎ Alerta! Insira um número valido de 1 a 4 e tente novamente!')
+        else:
+            print('⚠︎ Alerta! Insira um número valido de 1 a 4 e tente novamente!')
+
+# Inicar o programa:
+main()
---
+++ Atividade 5 - 03-06-24/Painel_Restaurante.py
@@ -0,0 +1,197 @@
+# Atividade: 
+# Descrição: 
+# Crie um aplicativo de cadastro de restaurante em Python. 
+# O programa precisará conter as seguintes informações: 
+# ▶ Restaurante: 
+# ▶ Nome 
+# ▶ Categoria (Pizzaria, Japonesa, Fastfood, Churrascaria...) 
+# ▶ Status (Ativado ou Desativado) 
+# O programa precisará conter as seguintes informações Funções: 
+# ▶ Função main() 
+# ▶ Exibir nome do aplicativo 
+# ▶ Exibir opções iniciais 
+# ▶ Retornar ao menu principal 
+# ▶ Finalizar o App 
+# ▶ Opção inválida (caso seja selecionado uma opção inexistente) 
+# ▶ Cadastrar o restaurante 
+# ▶ Listar restaurantes 
+# ▶ Alterar estado do restaurante(Ativar ou Desativar)
+# Instrutor: Brunno
+# Data: 03/06/2024
+# Autor: Higor Stanley aka Devyat009
+# Programa: Painel de gerenciador de estabelecimentos
+
+# Classe Restaurante
+class Restaurante:
+    def __init__(self, nome, categoria, status):
+        self.nome = nome
+        self.categoria = categoria
+        self.status = status
+
+# Função para checar se o input do usuário é número:
+def e_numero(string):
+    try:
+        if string.isnumeric():
+            string = int(string)
+            return True   
+        else:
+            string = float(string)
+            return False # Colocamos False para termos apenas valores inteiros
+    except ValueError:
+        return False
+
+# Função para cadastrar:
+def cadastrar_restaurantes(restaurantes):
+    while True:
+        nome = input('▶ Digite o nome do estabelecimento: ')
+        categoria = input('▶ Digite qual é a categoria do estabelecimento (FastFood, Pizzaria, Restaurante): ')
+        status = input('▶ Qual o status do estabelecimento, ele está ativado? (sim ou não): ').lower()
+        if status == 'sim' or status == 's':
+            status = True
+        else:
+            status = False
+        restaurante = Restaurante(nome, categoria, status)
+        restaurantes.append(restaurante)  # adicionamos o estabelecimento à lista
+        print('◇ Cadastro realizado com SUCESSO! ✓')
+        
+        continuar = input("◇ Deseja adicionar mais estabelecimentos? (s/n): ").lower()
+        if continuar != 's':
+            break
+    return restaurantes
+
+# Lista de Restaurantes:
+def listar_restaurantes(restaurantes):
+    # Caso não tenha nenhum estabelecimento:
+    if not restaurantes:
+        print('⋄ Não tem nenhum estabelecimento cadastrado')
+        return
+    # Imprimimos a lista de estabelecimento cadastrados:
+    # Titulo da lista
+    print('  ESTABELECIMENTO'.ljust(20) + '▎' + '  NOME'.ljust(20) + '▎' + '  CATEGORIA'.ljust(20) + '▎' + '  STATUS'.ljust(10))
+    # Itens numerados e ajustados de acordo com o titulo:
+    for i, restaurante in enumerate(restaurantes, start=1):
+        nome = restaurante.nome
+        categoria = restaurante.categoria
+        # Convertemos o True e False para palavra
+        status = "ATIVADO" if restaurante.status else "DESATIVADO" 
+        print(f'Estabelecimento {i}'.ljust(20) + '▎' + f'{nome}'.ljust(20) + '▎' + f'{categoria}'.ljust(20) + '▎' + f'{status}'.ljust(10))
+
+# Alterar o Status
+def alterar_status(restaurantes):
+    # Listamos os estabelecimentos para que o usuario possa escolher oque deseja alterar o estado:
+    listar_restaurantes(restaurantes)
+    # Caso não tenha nenhum 
+    if not restaurantes:
+        return
+    # Caso tenha, tentar:
+    try:
+        # Usuario escolhe o estabelecimento alvo da lista:
+        rest_alvo = int(input('▶ Digite o número do estabelecimento em que deseja alterar o estado: '))
+        # Se numero alvo do estabelecimento for menor que 0
+        if 0 < rest_alvo <= len(restaurantes):
+            # Usuario inseri o novo estado:
+            status = input('▶ Insira o novo status, Ativado ou Desativado (sim ou não): ')
+            if status == 'sim' or status == 's':
+                status = True
+            else:
+                status = False
+            # Usamos rest_alvo - 1 pois o for começa do 0
+            restaurantes[rest_alvo - 1].status = status
+            print(f'⋄ O estado do estabelecimento {restaurantes.nome} foi alterado com SUCESSO! ✓')
+        else:
+            print('⚠︎ Insira um valor valido!')
+    except ValueError: # Exceção de erro para a tentativa:
+        print('⚠︎ Insira um valor valido!')
+
+def remover_estabelecimento(lista):
+    # Listamos os estabelecimentos para que o usuario possa escolher oque deseja alterar o estado:
+    listar_restaurantes(lista)
+    # Caso não tenha nenhum 
+    if not lista:
+        return
+    # Caso tenha, tentar:
+    try:
+        # Usuario escolhe o estabelecimento alvo da lista:
+        rest_alvo = int(input('▶ Digite o número do estabelecimento em que deseja remover: '))
+        # Se numero alvo do estabelecimento for menor que 0
+        if 0 < rest_alvo <= len(lista):
+            print('⋄ Você confirma sua escolha S para SIM ou N para NÃO, LEMBRE-SE NÃO É POSSÍVEL REVERTER')
+            escolha = input('▶ Escolha: ').lower()
+            if escolha == 's':
+                # Usamos rest_alvo - 1 pois o for começa do 0
+                estabelecimento_removido = lista.pop(rest_alvo - 1)
+                print(f'⋄ O estabelecimento "{estabelecimento_removido.nome}" foi removido com SUCESSO! ✓')
+            elif escolha == 'n':
+                print('⋄ Cancelando...')
+            else:
+                print('⚠︎ Insira um valor valido!')
+    except ValueError:
+        print('⚠︎ Insira um valor valido!')
+
+# Nome do Programa:
+def nome_programa():
+    print('''
+         Ｓｔｏｒｅ　Ｍａｎａｇｅｒ
+         ✦•┈๑⋅⋯ ( ˶ˆ꒳ˆ˵ ) ⋯⋅๑┈•✦ ''')
+
+# Opções Inico:
+def opcoes_inicio():
+    print('┏━╾──────────────────────────────────╼━┓')
+    print('┃〔 1  ＋Cadastrar restaurantes      〕┃')
+    print('┃〔 2  ☰ Listar Restaurantes         〕┃')
+    print('┃〔 3  ☲ Opções                      〕┃')
+    print('┃〔 4  ⤾ Sair do aplicativo          〕┃')
+    print('┗━╾──────────────────────────────────╼━┛')
+
+# Opções para as opções:
+def opcoes_opcoes(restaurantes):
+    while True:
+        print('┏━╾──────────────────────────────────────────╼━┓')
+        print('┃〔 1  ✎  Alterar o estado de funcionamento  〕┃')
+        print('┃〔 2  ✗  Deletar um estabelecimento         〕┃')
+        print('┃〔 3  ⇦  Voltar ao menu principal           〕┃')
+        print('┗━╾──────────────────────────────────────────╼━┛')
+        
+        escolha = input('▶ Sua escolha é: ')
+        if e_numero(escolha):
+            escolha = int(escolha)
+            match escolha:
+                case 1:
+                    alterar_status(restaurantes)
+                case 2:
+                    remover_estabelecimento(restaurantes)
+                case 3:
+                    # Sair do loop e voltar ao menu principal
+                    break
+                case _:
+                    print('⚠︎ Opção invalida, tente novamente!')
+        else:
+            print('⚠︎ Insira um número correspondente a uma opção válida!')
+
+# Main:
+def main():
+    restaurantes = []
+    while True:
+        nome_programa()
+        opcoes_inicio()
+        escolha_opcao = input('▶ Sua escolha é: ')
+        if e_numero(escolha_opcao):
+            escolha_opcao = int(escolha_opcao)
+            match escolha_opcao:
+                case 1:
+                    cadastrar_restaurantes(restaurantes)
+                case 2:
+                    listar_restaurantes(restaurantes)
+                case 3:
+                    opcoes_opcoes(restaurantes)
+                case 4:
+                    print('⋄ Saindo.....')
+                    return  # Sair da função main e terminar o programa:
+                case _:
+                    print('⚠︎ Alerta! Insira um número valido de 1 a 4 e tente novamente!')
+        else:
+            print('⚠︎ Alerta! Insira um número valido de 1 a 4 e tente novamente!')
+
+# Chamamos main:
+if __name__ == '__main__':
+    main()
