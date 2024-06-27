dd7ea948d4e93505ea04d57742db50ce41f57d5d
devyat009 <91706455+devyat009@users.noreply.github.com>
2024-06-27 18:34:10 -0300

    Directory change

--- UC4 - Programação Orientada a Objetos/Atividade - 04/App.py
+++ UC4 - Programação Orientada a Objetos/Atividade - 04/App.py
@@ -1,3 +1,4 @@
+#Atualizações nos atalhos do teclado … Em quinta-feira, 1 de agosto de 2024, os atalhos de teclado do Drive serão atualizados para oferecer a navegação por letras iniciais.Saiba mais
 # Descrição:
 # Crie um sistema de cadastro de restaurante e seu respectivo cardápio em Python utilizando Programação
 # Orientada a Objetos (POO).
@@ -7,12 +8,14 @@
 # • Restaurante:
 #     o nome
 #     o Categoria (Pizzaria, Japonesa, Fastfood, Churrascaria...)
+
+# Imports
 from modelos.Restaurante import Restaurante
+from modelos.cardapio.ItemCardapio import ItemCardapio
+from modelos.cardapio.Bebida import  Bebida
 
 class App():
     """
-    Informações:
-    ------------
     Serve como o programa em si para chamar as classes
     
     Versão: 1.1
@@ -50,6 +53,13 @@ ______          _                              _
 \_| \_\___||___/\__\__,_|\__,_|_|  \__,_|_| |_|\__\___|
 ''')
 
+    def opcoes_inicio():
+        print(f'┏{('━'*32).ljust(25)}┓')
+        print(f'┃ {('〔 1 - GG EZ〕 ').ljust(29)}┃')
+        print(f'┃ {('〔 2 - GG EZ PZ〕 ').ljust(29)}┃')
+        print(f'┃ {('〔 3 - GG EZ PZ LMN SQZY 〕').ljust(29)}┃')
+        print(f'┃ {('〔 4 - GG EZ PZ LEMON SQUEZYY〕').ljust(29)}┃')
+        print(f'┗{('━'*32).ljust(25)}┛')
 
 print('─'*67)
 KiGordo = Restaurante('KiGordo', 'FastFood')
@@ -58,7 +68,17 @@ ChurradoSul = Restaurante('Churras do Sul', 'Churrascaria')
 ChurradoSul.alterar_status()
 Restaurante.listar_restaurantes()
 
-print(Restaurante.lista_restaurantes)
-print(ChurradoSul)
-Restaurante.listar_restaurantes()
 # print('─'*50)
+#sucudipera = Bebida('Suco di Pera', '250ml', 12.00)
+#print(sucudipera)
+print(App.opcoes_inicio())
+
+print('─'*67)
+suco = ItemCardapio('Suco Laranja', 6.00)
+suco2 = Bebida('Laranjola', '300ml', 12.00)
+suco1 = Bebida('Vodka Kadov', '1L', 35.00)
+print(suco)
+print(suco2)
+print('─'*67)
+ChurradoSul.adicionar_no_cardapio(suco1)
+ChurradoSul.mostrar_cardapio()
